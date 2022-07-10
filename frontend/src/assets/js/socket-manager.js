dc91ab170edb21a7f1daa153eb1150260644117a
jenesius <lokargenia@gmail.com>
2022-07-11 02:12:22 +0300

    Update socket dev url

--- frontend/src/assets/js/socket-manager.js
+++ frontend/src/assets/js/socket-manager.js
@@ -1,6 +1,6 @@
 import {Manager} from "socket.io-client";
 
-const manager = new Manager("http://localhost:3001", {
+const manager = new Manager(process.env.NODE_ENV === 'development' ? "http://localhost:3001": '/', {
 	withCredentials: true
 });
 
